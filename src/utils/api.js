4853ceb5dc008531fecef8b4fa55301b4a464cfa
Rajeev Kumar <rajeev02030066@gmail.com>
2024-01-14 11:47:09 +0530

    New Commit File

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,8 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+        "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
+            // process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
@@ -15,3 +15,5 @@ export const fetchDataFromApi = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     return data;
 };
+
+
